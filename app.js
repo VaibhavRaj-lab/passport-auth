1647bee034d3981bcd881f7c896fac3d8c10e08f
vaibhav <vaibhav456654@gmail.com>
2020-12-17 12:45:13 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require(`express`)
+
+const app = express();
+
+const expressLayouts = require(`express-ejs-layouts`)
+ 
+const mongoose = require(`mongoose`)
+const passport = require('passport');
+
+const bodyparser = require(`body-parser`)
+const flash = require('connect-flash');
+const session = require('express-session');
+// Passport Config
+require('./config/passport')(passport);
+// db config 
+const db = require(`./config/key`).MongoURI;
+// connect mongo 
+mongoose.connect( "mongodb+srv://vaibhavpa:dmtUxO1tVtvwTHxI@cluster0.rk1ef.mongodb.net/vaibhavpa?retryWrites=true&w=majority"
+, {useNewUrlParser:true , useUnifiedTopology: true   })
+.then(()=>console.log("..mongo connected"))
+.catch(err=> console.log(err)) ;
+
+// EJS
+app.use(expressLayouts)
+app.set(`view engine`, `ejs`)
+
+// bodyparser
+app.use( bodyparser.urlencoded({extended: true}))
+// Express session
+app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    })
+  );
+  
+  // Passport middleware
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+  // Connect flash
+  app.use(flash());
+  
+  // Global variables
+  app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+  });
+// routes  
+const index = require(`./Routes/index.js`)
+const users = require(`./Routes/users.js`)
+
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+const PORT= process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`server is listening at ${PORT}`)})
+
---
+++ config/key.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://vaibhavpa'+ encodeURIComponent(`shddQT0LWTDKlDVe`) + 'vaibhavpa.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: dbPassword
+};
